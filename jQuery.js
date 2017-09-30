4a0e4465e261fd3c5af7b33a238d4183bf1a6800
Mankirat Singh <mankirat1998@gmail.com>
2017-09-30 10:55:35 +0530

    Horizontal Fruit throwing added

--- jQuery.js
+++ jQuery.js
@@ -4,6 +4,8 @@ var trialsLeft;
 var fruits= ['apple','banana','grapes','orange','peach','pineapple','pumpkin','raspberry','strawberry','tomato'];
 var step;
 var action;
+var type;
+var up;
 
 $(function () {
     $("#start").click(function () {
@@ -48,41 +50,131 @@ $(function () {
     function startAction() {
         $("#fruit1").show();
         chooseFruits();
-        $("#fruit1").css({
-            'left': Math.round(Math.random()*600),
-            'top':-80
-        })
+        type = Math.round(Math.random());
+        up=true;
 
-        step= Math.round(Math.random()*6)+1;
+        if(type==0) {
+            $("#fruit1").css({
+                'left': Math.round(Math.random() * 600),
+                'top': -80
+            })
+        }else{
+            $("#fruit1").css({
+                'left': -50,
+                'top': 300
+            })
+        }
+
+        // step= Math.round(Math.random()*6)+1;
+        step=8;
 
         action = setInterval(function () {
-            $("#fruit1").css('top',$("#fruit1").position().top+step)
+            if(type==0) {
+                $("#fruit1").css('top', $("#fruit1").position().top + step)
 
-            if($("#fruit1").position().top > $("#fruitContainer").height()){
-                if(trialsLeft > 1){
+                if ($("#fruit1").position().top > $("#fruitContainer").height()) {
+                    if (trialsLeft > 1) {
 
-                    chooseFruits();
-                    $("#fruit1").css({
-                        'left': Math.round(Math.random()*600),
-                        'top':-80
-                    })
+                        type = Math.round(Math.random());
+                        up=true;
 
-                    step= Math.round(Math.random()*6)+1;
+                        if(type==0) {
+                            $("#fruit1").css({
+                                'left': Math.round(Math.random() * 600),
+                                'top': -80
+                            })
+                        }else{
+                            $("#fruit1").css({
+                                'left': -50,
+                                'top': 300
+                            })
+                        }
 
-                    trialsLeft--;
-                    addHearts();
-                }else{
-                    play=false;
-                    $("#start").html("Start Game");
-                    $("#trialsLeftBox").hide();
-                    $("#gameOver").show();
-                    $("#gameOver").html("<p>Game Over</p><p>Your Score is "+score+"</p>");
-                    $("#trialsLeftBox").hide();
-                    stopAction();
+                        // step = Math.round(Math.random() * 6) + 1;
+                        step=8;
 
+                        trialsLeft--;
+                        addHearts();
+                    } else {
+                        play = false;
+                        $("#start").html("Start Game");
+                        $("#trialsLeftBox").hide();
+                        $("#gameOver").show();
+                        $("#gameOver").html("<p>Game Over</p><p>Your Score is " + score + "</p>");
+                        $("#trialsLeftBox").hide();
+                        stopAction();
+
+                    }
                 }
             }
-        },12)
+            else{
+                if($("#fruit1").position().top> 100 && up==true){
+                    var n=1;
+                    if($("#fruit1").position().top<250 && $("#fruit1").position().top>150){
+                        n=2;
+                    }else if ($("#fruit1").position().top<150 && $("#fruit1").position().top>120){
+                        n=4;
+                    }else if($("#fruit1").position().top<120){
+                        n=8;
+                    }
+                    temp= step/n;
+                    $("#fruit1").css('top', $("#fruit1").position().top - temp);
+
+                    console.log($("#fruit1").position().top);
+
+                    if((100-$("#fruit1").position().top)<step && $("#fruit1").position().top< 110){
+                        up=false;
+                    }
+                }else{
+                    var n=1;
+                    if($("#fruit1").position().top<250 && $("#fruit1").position().top>150){
+                        n=2;
+                    }if ($("#fruit1").position().top<150 && $("#fruit1").position().top>120){
+                        n=4;
+                    }if($("#fruit1").position().top<120){
+                        n=8;
+                    }
+
+                    $("#fruit1").css('top', $("#fruit1").position().top + step/n);
+                }
+                $("#fruit1").css('left', $("#fruit1").position().left + step);
+
+                if ($("#fruit1").position().top > $("#fruitContainer").height() || $("#fruit1").position().left > $("#fruitContainer").width()) {
+                    if (trialsLeft > 1) {
+
+                        type = Math.round(Math.random());
+                        up=true;
+
+                        if(type==0) {
+                            $("#fruit1").css({
+                                'left': Math.round(Math.random() * 600),
+                                'top': -80
+                            })
+                        }else{
+                            $("#fruit1").css({
+                                'left': -50,
+                                'top': 300
+                            })
+                        }
+
+                        // step = Math.round(Math.random() * 6) + 1;
+                        step=8;
+
+                        trialsLeft--;
+                        addHearts();
+                    } else {
+                        play = false;
+                        $("#start").html("Start Game");
+                        $("#trialsLeftBox").hide();
+                        $("#gameOver").show();
+                        $("#gameOver").html("<p>Game Over</p><p>Your Score is " + score + "</p>");
+                        $("#trialsLeftBox").hide();
+                        stopAction();
+
+                    }
+                }
+            }
+        },24)
     }
 
     function stopAction() {
